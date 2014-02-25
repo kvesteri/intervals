000457831e62baf3f70d96f004e9a8a0c90edad0
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2014-02-25 12:39:37 +0200

    Bump version

--- intervals/__init__.py
+++ intervals/__init__.py
@@ -25,7 +25,7 @@ __all__ = (
 )
 
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 
 def is_number(number):
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ extras_require = {
 
 setup(
     name='intervals',
-    version='0.2.2',
+    version='0.2.3',
     url='https://github.com/kvesteri/intervals',
     license='BSD',
     author='Konsta Vesterinen',
